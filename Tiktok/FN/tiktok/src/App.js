bd2ef55ec062cdc667cd3ad68bde1de004eefc7e
duypk <duypk@unit.com.vn>
2023-12-19 13:38:18 +0700

    Dựng khung Layout mặc định

--- Tiktok/FN/tiktok/src/App.js
+++ Tiktok/FN/tiktok/src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, lazy } from 'react';
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
 import { publicRoutes } from '~/routes'
 import { DefaultLayout } from '~/components/Layout';
