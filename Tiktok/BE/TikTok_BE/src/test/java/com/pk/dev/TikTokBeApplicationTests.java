ed64320ffdd67da344d7aa8c44d31baa696087a0
duypk <duypk@unit.com.vn>
2023-12-19 09:30:14 +0700

    create src code API BE

---
+++ Tiktok/BE/TikTok_BE/src/test/java/com/pk/dev/TikTokBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikTokBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
